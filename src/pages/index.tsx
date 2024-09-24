ff377b66eb83d86a757646f070d4ea2b5840f890
aristotleee <148852099+aristotleee@users.noreply.github.com>
2024-09-24 10:59:10 +0800

    minor optimization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,11 @@ import { Page } from "@/components/Page";
 
 export default function Home() {
   return (
-    <>
-      <Page>
-        <main>
-          <h1>A 2-way peg bridging BTC to Ethereum</h1>
-          <h3>Once bridged, your BTC becomes eBTC, an ERC20 token backed 1:1 by Bitcoin. Your deposit is secured by immutable code, not centralized custodians, and can be redeemed anytime.</h3>
-        </main>
-      </Page>
-    </>
+    <Page>
+      <main>
+        <h1>A 2-way peg bridging BTC to Ethereum</h1>
+        <h3>Once bridged, your BTC becomes eBTC, an ERC20 token backed 1:1 by Bitcoin. Your deposit is secured by immutable code, not centralized custodians, and can be redeemed anytime.</h3>
+      </main>
+    </Page>
   );
 }
